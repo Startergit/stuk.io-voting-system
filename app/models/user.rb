606c39a517939594f875c150557bc800c8b26b19
David Pan <codingisfun26@gmail.com>
2016-02-14 19:45:53 -0800

    created upvote/downvote buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	act_as_voter
+	acts_as_voter
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
